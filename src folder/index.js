0c10eae30b2d4f5115ebf47d2e6509b269eb6afc
rinitsi <banitsirachel@gmailcom>
2024-12-15 19:18:44 +0000

    added more code

--- src folder/index.js
+++ src folder/index.js
@@ -1,11 +1,14 @@
-function checkGender(){
-    let gender = prompt("What is your gender ");
-    if (gender == "female"){
-        alert(`You are a girl 👧!`);
-    }else if(gender == "male"){
-        alert(`You are a boy!👦`);
-    }else {
-        alert(`Enter your gender`);
-    }
-}
-checkGender();
+// function checkGender(){
+//     let gender = prompt("What is your gender ");
+//     if (gender == "female"){
+//         alert(`You are a girl 👧!`);
+//     }else if(gender == "male"){ 
+//         alert(`You are a boy!👦`);
+//     }else {
+//         alert(`Enter your gender`);
+//     }
+// }
+// checkGender();
+
+
+
